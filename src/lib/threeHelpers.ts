4c1870f81dd5af45fc614337d7b101846030822c
Isaac Druin <isaac.druin@gmail.com>
2024-11-03 13:50:28 -0600

    finish chapter 1.4 - transformation of objects & grouping

--- src/lib/threeHelpers.ts
+++ src/lib/threeHelpers.ts
@@ -5,14 +5,14 @@ export function createScene() {
     return scene;
 }
 
-export function createCamera() {
+export function createCamera(x: number, y: number, z: number) {
     const camera = new THREE.PerspectiveCamera(
  	   75,
  	   window.innerWidth / window.innerHeight,
  	   0.1,
  	   1000
     );
-    camera.position.z = 5;
+    camera.position.set(x, y, z);
     return camera;
 }
 
@@ -22,10 +22,15 @@ export function createRenderer() {
     return renderer;
 }
 
-export function createCube(scene: THREE.Scene) {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+export function createCube(x: number, y: number, z: number, color: THREE.ColorRepresentation) {
+    const geometry = new THREE.BoxGeometry(x, y, z);
+    const material = new THREE.MeshBasicMaterial({ color });
     const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
     return cube;
+}
+
+export function createAxisHelper(scene: THREE.Scene) {
+    const axesHelper = new THREE.AxesHelper();
+    scene.add(axesHelper);
+    return axesHelper;
 }
--- src/routes/chapter1/+page.svelte
+++ src/routes/chapter1/+page.svelte
@@ -1,29 +1,46 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { createScene, createCamera, createRenderer, createCube } from '$lib/threeHelpers';
+	import {
+		createScene,
+		createCamera,
+		createRenderer,
+		createCube,
+		createAxisHelper
+	} from '$lib/threeHelpers';
 	import * as THREE from 'three';
 
 	let threeContainer: HTMLElement | null = null;
 
 	onMount(() => {
 		const scene = createScene();
-		const camera = createCamera();
+		const camera = createCamera(1, 2, 5);
 		const renderer = createRenderer();
+		const axisHelper = createAxisHelper(scene);
 
 		if (threeContainer) {
 			threeContainer.appendChild(renderer.domElement);
 		}
 
-		const cube = createCube(scene);
+		const group = new THREE.Group();
+		group.position.set(0, 0, 0);
+
+		const cube = createCube(1, 1, 1, 'red');
+		const cube2 = createCube(1, 1, 1, 'green');
+		const cube3 = createCube(1, 1, 1, 'blue');
+
+		cube2.position.x = 3;
+		cube3.position.x = -3;
+
+		group.add(cube, cube2, cube3);
+		scene.add(group);
 
 		function animate() {
-			cube.rotation.x += 0.01;
-			cube.rotation.y += 0.01;
+			group.rotation.z += 0.01;
 			renderer.render(scene, camera);
 		}
 
 		renderer.setAnimationLoop(animate);
-
+		camera.lookAt(cube.position);
 		const onResize = () => {
 			camera.aspect = window.innerWidth / window.innerHeight;
 			camera.updateProjectionMatrix();
