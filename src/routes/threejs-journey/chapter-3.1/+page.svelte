f13976e687d2d72bbbe83ccc99a878e5cc60f0ed
RiskTolerance <isaac.druin@gmail.com>
2025-02-27 14:04:37 -0600

    finish physics!

--- src/routes/threejs-journey/chapter-3.1/+page.svelte
+++ src/routes/threejs-journey/chapter-3.1/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import * as R from '@dimforge/rapier3d-compat';
 	import * as T from 'three';
-	import { onMount } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	import { OrbitControls, RGBELoader } from 'three/examples/jsm/Addons.js';
 	import Stats from 'three/addons/libs/stats.module.js';
 	import hdr from '$lib/assets/hdrs/chinese_garden_2k.hdr';
@@ -36,7 +36,6 @@
 			}
 			const sceneObjects: SceneObject[] = [];
 
-			console.log(hitSoundFile);
 			const hitSound = new Audio(hitSoundFile);
 			// hitSound.play().then(() => {
 			// 	console.log('played');
@@ -76,12 +75,13 @@
 				});
 			};
 
+			interface cubeDims {
+				x: number;
+				y: number;
+				z: number;
+			}
 			const createCube = (
-				dimensions: {
-					x: number;
-					y: number;
-					z: number;
-				} = { x: 1, y: 1, z: 1 },
+				dimensions: cubeDims = { x: 1, y: 1, z: 1 },
 				position: T.Vector3 = new T.Vector3(0, 0, 0),
 				bounce: number = 1
 			) => {
@@ -99,11 +99,9 @@
 					.setCanSleep(true);
 				const body = world.createRigidBody(bodyDesc);
 				// collider
-				const colliderDesc = R.ColliderDesc.cuboid(
-					dimensions.x,
-					dimensions.y,
-					dimensions.z
-				).setActiveEvents(R.ActiveEvents.CONTACT_FORCE_EVENTS);
+				const colliderDesc = R.ColliderDesc.cuboid(dimensions.x, dimensions.y, dimensions.z)
+					.setRestitution(bounce)
+					.setActiveEvents(R.ActiveEvents.CONTACT_FORCE_EVENTS);
 				const collider = world.createCollider(colliderDesc, body);
 
 				sceneObjects.push({
@@ -122,6 +120,7 @@
 					);
 					createSphere(randDiameter, randPos);
 				});
+
 				addCubeButton.addEventListener('click', () => {
 					const randDim1 = Math.max(Math.random(), 0.1);
 					const randDim2 = Math.max(Math.random(), 0.1);
@@ -217,6 +216,10 @@
 
 			const eventQueue = new R.EventQueue(true);
 
+			const clamp = (num: number, min: number, max: number) => {
+				return num <= min ? min : num >= max ? max : num;
+			};
+
 			const animate = () => {
 				const elapsedTime = clock.getElapsedTime();
 				const deltaTime = elapsedTime - oldElapsed;
@@ -232,8 +235,13 @@
 
 				eventQueue.drainContactForceEvents((e) => {
 					const force = e.maxForceMagnitude();
-					if (e.maxForceMagnitude() > 100) {
-						console.log('play!');
+					if (force > 100) {
+						// clamp the force between 50 and 5000
+						// then set the volume between 0 and 1:
+						const volume = clamp(force, 100, 4000) / 4000;
+						console.log(volume);
+						hitSound.volume = volume;
+						hitSound.currentTime = 0;
 						hitSound.play();
 					}
 				});
@@ -250,15 +258,15 @@
 	});
 </script>
 
-<div
-	class="relative flex h-[calc(100vh-56px)] w-full items-center justify-center overflow-clip"
-	bind:this={threeContainer}
->
+<div class="relative">
+	<div
+		class="z-0 flex h-[calc(100vh-56px)] w-full items-center justify-center overflow-clip"
+		bind:this={threeContainer}
+	></div>
 	<div class="absolute left-0 top-0 flex h-20 w-fit gap-4 p-4">
 		<button class="rounded-md border p-2 text-white backdrop-blur-md" bind:this={addSphereButton}
 			>Add Sphere</button
 		>
-
 		<button class="rounded-md border p-2 text-white backdrop-blur-md" bind:this={addCubeButton}
 			>Add Cube</button
 		>
